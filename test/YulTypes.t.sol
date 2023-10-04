7655407a6c112d8269ce573755d5633f7ed9fb6f
Angry Coding <mailidanielifechukwu@gmail.com>
2023-10-04 19:30:01 +0100

    tested string bytes32 bytes yul

--- test/YulTypes.t.sol
+++ test/YulTypes.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import "../src/YulTypes.sol";
 
 contract YulTypesTest is Test {
@@ -29,5 +30,23 @@ contract YulTypesTest is Test {
     function testGetReprentation() public {
         address rep = types.getReprentation();
         assertEq(rep, address(1));
+    }
+
+    function testCheckProofShitValues() public {
+        bool val1;
+        bool val2;
+        (val1, val2) = types.proofBoolZeroShiftByte();
+        assertEq(val1, true);
+        assertEq(val2, false);
+    }
+
+    function testYulString() public {
+        string memory expected = "Hello World";
+        bytes memory message = bytes(types.getString());
+
+        assertEq(
+            bytes32(abi.encodePacked(message)),
+            bytes32(abi.encodePacked(expected))
+        );
     }
 }
